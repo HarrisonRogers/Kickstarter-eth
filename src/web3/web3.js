098f6e17404dfda237323ba2e21cd05c2ae93993
harrison rogers <harrison.rogers05@gmail.com>
2024-11-30 14:39:41 +1300

    update web3 file

--- src/web3/web3.js
+++ src/web3/web3.js
@@ -1,8 +1,16 @@
 import Web3 from 'web3'
 
 // Assumption that the browser has metamask installed
-window.ethereum.request({ method: 'eth_requestAccounts' })
+let web3
 
-const web3 = new Web3(window.ethereum)
+if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
+  window.ethereum.request({ method: 'eth_requestAccounts' })
+  web3 = new Web3(window.ethereum)
+} else {
+  const provider = new Web3.providers.HttpProvider(
+    process.env.NEXT_PUBLIC_INFURA_ENDPOINT
+  )
+  web3 = new Web3(provider)
+}
 
 export default web3
