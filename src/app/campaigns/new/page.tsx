0a6f4d85a5f06dc152716429eea5bbbcfd8f1916
harrison rogers <harrison.rogers05@gmail.com>
2024-12-21 12:11:53 +1300

    add zod form validition to the create campaign form

--- src/app/campaigns/new/page.tsx
+++ src/app/campaigns/new/page.tsx
@@ -7,35 +7,62 @@ import { getFactory } from '@/web3/factory';
 import web3 from '@/web3/web3';
 import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { Textarea } from '@/components/ui/textarea';
+
+const schema = z.object({
+  campaignTitle: z.string().min(1, { message: 'Campaign title is required' }),
+  campaignDescription: z
+    .string()
+    .min(1, { message: 'Campaign description is required' }),
+  minimumContribution: z
+    .string()
+    .min(1, { message: 'Minimum contribution is required' })
+    .refine((value) => !isNaN(Number(value)) && Number(value) >= 0.0001, {
+      message: 'Minimum contribution must be at least 0.0001 ETH',
+    }),
+});
+
+type CampaignFormData = z.infer<typeof schema>;
 
 function NewPage() {
-  const [minimumContribution, setMinimumContribution] = useState('');
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+  } = useForm<CampaignFormData>({
+    resolver: zodResolver(schema),
+  });
   const [error, setError] = useState('');
-  const [loading, setLoading] = useState(false);
   const [success, setSuccess] = useState(false);
   const router = useRouter();
 
-  const handleCreateCampaign = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
+  const handleCreateCampaign = async (data: CampaignFormData) => {
     try {
-      setLoading(true);
       const factoryInstance = await getFactory();
       const accounts = await web3.eth.getAccounts();
+      const minimumContributionInWei = web3.utils.toWei(
+        data.minimumContribution,
+        'ether'
+      );
 
       const result = await factoryInstance.methods
-        .createCampaign(minimumContribution)
+        .createCampaign(
+          minimumContributionInWei,
+          data.campaignTitle,
+          data.campaignDescription
+        )
         .send({
           from: accounts[0],
         });
       if (result) {
-        setLoading(false);
         setSuccess(true);
         router.push('/');
       }
     } catch (error) {
       console.error('Error creating campaign:', error);
-      setLoading(false);
       setError((error as Error).message);
     }
   };
@@ -45,21 +72,59 @@ function NewPage() {
       <h1 className="text-2xl font-bold">Create New Campaign</h1>
 
       <div className="flex mt-10 flex-col w-full max-w-lg">
-        <form className="space-y-4" onSubmit={handleCreateCampaign}>
-          <div className="flex flex-col gap-2">
-            <Label htmlFor="minimum-contribution">
-              Minimum Contribution (wei)
-            </Label>
-            <Input
-              placeholder="Amount of wei..."
-              id="minimum-contribution"
-              value={minimumContribution}
-              onChange={(e) => setMinimumContribution(e.target.value)}
-            />
+        <form
+          className="space-y-4"
+          onSubmit={handleSubmit(handleCreateCampaign)}
+        >
+          <div className="flex flex-col gap-7">
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="campaign-title">Campaign Title</Label>
+              <Input
+                required
+                placeholder="Campaign Title"
+                id="campaign-title"
+                {...register('campaignTitle')}
+              />
+              {errors.campaignTitle && (
+                <p className="text-red-500 text-sm">
+                  {errors.campaignTitle.message}
+                </p>
+              )}
+            </div>
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="campaign-description">Campaign Description</Label>
+              <Textarea
+                required
+                placeholder="Campaign Description"
+                id="campaign-description"
+                {...register('campaignDescription')}
+              />
+              {errors.campaignDescription && (
+                <p className="text-red-500 text-sm">
+                  {errors.campaignDescription.message}
+                </p>
+              )}
+            </div>
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="minimum-contribution">
+                Minimum Contribution (0.0001 ETH)
+              </Label>
+              <Input
+                required
+                placeholder="Amount of eth..."
+                id="minimum-contribution"
+                {...register('minimumContribution')}
+              />
+              {errors.minimumContribution && (
+                <p className="text-red-500 text-sm">
+                  {errors.minimumContribution.message}
+                </p>
+              )}
+            </div>
+            <Button type="submit" className="w-full" disabled={isSubmitting}>
+              {isSubmitting ? 'Creating...' : 'Create'}
+            </Button>
           </div>
-          <Button type="submit" className="w-full" disabled={loading}>
-            {loading ? 'Creating...' : 'Create'}
-          </Button>
           {error && <p className="text-coral">{error}</p>}
           {success && <p className="italic">Campaign created successfully</p>}
         </form>
